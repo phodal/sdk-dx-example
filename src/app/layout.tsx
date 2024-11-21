155ec51a16704f0491897cd9e8d5115a1b32f56c
Fengda Huang <h@phodal.com>
2024-11-21 09:05:57 +0800

    Remove unused import and update AntHeader component
    * Remove the unused import of `Typography` from `src/app/layout.tsx`
    * Update the `AntHeader` component to remove the "Header" text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Layout, Typography } from "antd";
+import { Layout } from "antd";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import AntHeader from "./AntHeader";
 import AntSider from "./AntSider";
